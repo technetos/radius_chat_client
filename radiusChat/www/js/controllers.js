1a0d0f890377ac378d57bf8ee43b96f2097378d8
Kyle Fetterman <kfetterm@iupui.edu>
2016-02-21 08:40:12 -0600

    Was Able to show message bodies in the chat

--- radiusChat/www/js/controllers.js
+++ radiusChat/www/js/controllers.js
@@ -29,9 +29,10 @@ angular.module('radiusChat.controllers', [])
       function(res){
         if(res.status === 200){
           UserSession.set("user",res.data);
-          UserSession.set("authenticated", true);
+          UserSession.set("active", true);
           $state.go('chat');
         }
+        console.dir(res)
       },
        function(res){
          $state.go('error');
@@ -100,8 +101,54 @@ angular.module('radiusChat.controllers', [])
    });
   }
  })
- .controller('ChatCtrl', function($scope, UserSession){
-   
+ .controller('ChatCtrl', function($scope, $state, UserSession, socket){
+   if(!UserSession.get().active){
+     $state.go('login');
+   }
+
+   $scope.text;
+   $scope.geoLocation = {};
+   $scope.buffer = [];
+
+   socket.on('send:message', function (message) {
+     console.dir(message);
+     $scope.buffer.push(message);
+   });
+
+   $scope.sendMessage = function (text) {
+     geoLocation = {}
+
+     function _getGeoLocation() {
+         if (navigator.geolocation) {
+             navigator.geolocation.getCurrentPosition(_showPosition);
+         } else {
+             $state.go('error');
+         }
+     }
+
+     function _showPosition(position) {
+         $scope.user.geoLocation.latitude = position.coords.latitude;
+         $scope.user.geoLocation.longitude = position.coords.longitude;
+     }
+
+     console.dir($scope.geoLocation);
+
+     socket.emit('send:message', {
+       message: {
+         text : text,
+         sender : UserSession.get().user.username,
+         geoLocation : {
+           latitude : $scope.geoLocation.latitude,
+           longitude : $scope.geoLocation.longitude
+         }
+       }
+     });
+     UserSession.setLocation(geoLocation);
+   }
+
+   function _inRange(messageGeoLocation){
+     //return (UserSession.get().radius <= (((UserSession.getLocation().longitude - messageGeoLocation.longitude)**2) + ((UserSession.getLocation().latitude - messageGeoLocation.latitute)**2)** .5)* .00001);
+   }
  })
  .controller('ErrorCtrl', function($scope, $ionicPopup, $state){
     var alertPopup = $ionicPopup.show({
--- radiusChat/www/js/services.js
+++ radiusChat/www/js/services.js
@@ -1,12 +1,41 @@
 angular.module('radiusChat.services', [])
 .factory('UserSession', function() {
-  var currentSessions = {};
+  var currentSessions = {active:false};
   return {
     get: function(){
       return currentSessions;
     },
     set: function(key,value){
       currentSessions[key] = value;
+    },
+    setLocation: function(geoLocation){
+      currentSessions.user.geoLocation = geoLocation;
+    },
+    getLocation: function(){
+      return currentSessions.user.geoLocation;
+    }
+  };
+})
+.factory('socket', function ($rootScope) {
+  var socket = io.connect('http://52.32.132.194:3000');
+  return {
+    on: function (eventName, callback) {
+      socket.on(eventName, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      });
+    },
+    emit: function (eventName, data, callback) {
+      socket.emit(eventName, data, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+      })
     }
   };
 });
